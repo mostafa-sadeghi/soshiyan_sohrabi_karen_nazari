c27d681ce240281b734cd6068e10b2c0ef7e1a19
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-11-22 16:58:50 +0330

    1 azar

---
+++ azar_1.py
@@ -0,0 +1,55 @@
+# import turtle
+
+# canvas = turtle.Screen()
+# canvas.bgcolor('orange')
+
+# p = turtle.Pen()
+# p.pensize(4)
+# p.pencolor('red')
+
+# p.forward(100)
+# p.left(90)
+
+# p.forward(100)
+# p.left(90)
+
+# p.forward(100)
+# p.left(90)
+
+# p.forward(100)
+# p.left(90)
+
+# canvas.exitonclick()
+# age = 15
+# if age > 18:
+#     print("adult")
+#     print("ok")
+# if age == 18:
+#     print("equal")
+#     print("ok")
+# if age != 18:
+#     print("not equal")
+#     print("not ok")
+
+name = "mohammad"
+
+if name == "ali":
+    print("ali")
+
+if name == "tina":
+    print("tina")
+
+if name == "mohammad":
+    print("mohammad")
+
+# exercise 1: کشیدن مثلث
+# exercise 2 : x = 10
+# if x > 10: greater  if x == 10 > ok if x < 10 lower
+
+
+print("this is sample which we want to test")
+x = 12
+if x == 12:
+    print("equal")
+if x > 12:
+    print("greater")

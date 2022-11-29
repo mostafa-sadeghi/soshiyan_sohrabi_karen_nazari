e4777537b41c6ff7bd19aacb15d6ebe1cf7636b3
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-11-29 16:56:56 +0330

    8azar

---
+++ azar_8.py
@@ -0,0 +1,39 @@
+# age = 12
+# if age>=18:
+#     print('Adult')
+# else:
+#     print('Kid')
+# age = 12
+# if age >= 18:
+#     print('Adult')
+# elif 12 <= age < 18:
+#     print('junior')
+# else:
+#     print('Kid')
+
+# number1 = input('enter first number ')
+# number2 = input('enter second number ')
+
+# x = int(number1) + int(number2)
+
+# print('sum of two numbers is :', x)
+
+# if x < 10:
+#     print('sum is lower than 10')
+# elif 10 < x < 20:
+#     print('sum is between 10 and 20')
+
+# elif x > 10 and x < 20:
+#     print('sum is between 10 and 20')
+
+
+# else:
+#     print('sum is greater than 10')
+
+
+x = input('enter a number: ')
+
+if x == '2' or x == '4':
+    print('ok')
+else:
+    print('not ok')

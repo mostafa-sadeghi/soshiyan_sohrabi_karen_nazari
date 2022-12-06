401acc171669ba00e9531bcc353d6f61110c3037
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-12-06 17:00:05 +0330

    15azar

---
+++ azar_15.py
@@ -0,0 +1,89 @@
+# x1 = range(10)  # 0 - 9
+# print(x1)
+# print(list(x1))  # print(list(range(10)))
+# print(tupl# x2 = range(10, 20)  # 10 - 19
+# print(x2)e(x1))  # print(tuple(range(10)))
+
+# for x in range(5):
+#     print(f'number {x}')
+#     print('**********')
+
+# name = ['ali', 'reza', 'matin', 'tina', 'sima']
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+# for n in name:
+#     print(n)
+
+# exercise1:
+'''
+با استفاده از حلقه عددهای زوج کمتر از صد را داخل یک لیست قرار بده و در پایان لیست را پرینت کن
+'''
+# if x % 2 == 0:
+
+# exercise2
+'''
+داخل لیست بالا یعنی name
+بگرد و در صورتی که نام متین را پیدا کردن پیغام مناسبی ر اپرینت کن
+با حلقه
+
+'''
+
+# x = range(0, 10, 2)
+# print(list(x))
+# for i in range(0,100,2):
+#     print(i)
+
+# name = ['a','b','c']
+
+# for i in name:
+#     print(i)
+#     for j in name:
+#         print(j)
+
+# for i in range(4):
+#     for j in range(4):
+#         print(j)
+#     print('**************************************')
+
+# exercise 3:
+
+'''
+برنامه ای بنویسید که یک عدد را از ورودی دریافت نماید و سپس
+به تعداد مشخصی ستاره چاپ نماید به این صورت که اگر کاربر مثلا عدد چنج را وارد نمود
+در خط اول یک ستاره
+در خط دوم دو ستاره
+در خط سوم سه ستاره
+در خط چهارم چهار ستاره 
+در خط پنجم پنج ستاره
+چاپ کند
+'''
+
+# excerise 4:
+'''
+برنامه بالا را به صورت برعکس انجام بده
+یعنی یک عدد از ورودی بگیر مثلا پنج
+در خط اول پنج ستاره 
+در خط دوم چهار ستاره
+در خط سوم سه ستاره
+در خط چهارم دو ستاره
+در خط پنجم یک ستاره نمایش دهد
+'''
+
+
+string = 'abc'
+for s in string:
+    print(s)
+
+numbers = (1, 2, 3)
+for n in numbers:
+    print(n)
+
+student = {
+    'name': 'ali',
+    'family': 'rezaei',
+    'age': 22
+
+}

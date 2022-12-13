58c35d29e4b46c3ff8087cbd44fbbd3fd9d1adc4
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-12-13 16:59:15 +0330

    22azar

--- azar_22.py
+++ azar_22.py
@@ -13,14 +13,14 @@
 #     print(i)
 
 
-# for i in range(1000,10001,2):
-#     print(i)
-# even_numbers = []
-# for i in range(1000, 10001):
-#     if i % 2 == 0:
-#         even_numbers.append(i)
-# print(even_numbers)
-# print(len(even_numbers))
+for i in range(1000, 10001, 2):
+    print(i)
+even_numbers = []
+for i in range(1000, 10001):
+    if i % 2 == 0:
+        even_numbers.append(i)
+print(even_numbers)
+print(len(even_numbers))
 
 # روش بعدی بدون کمک تابع len
 
@@ -31,3 +31,6 @@ for i in range(1000, 10001):
         count = count + 1
         numbers.append(i)
 print(count)
+
+# exercise1 : با دو روش بالا تعداد اعداد فرد بین دو هزار تا پنجاه هزار را نمایش بده
+# exercise 2: مجموع اعداد زوج بین دو هزار تا 3ه هزار را حساب نمایید

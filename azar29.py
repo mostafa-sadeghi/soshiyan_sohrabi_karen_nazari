1833453c81c5a68b16b0ce398568c2cb2b7b9291
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-12-20 16:59:01 +0330

    azar29

---
+++ azar29.py
@@ -0,0 +1,14 @@
+'enter student name'
+'enter python score'
+'enter app inventor score'
+'enter scratch score'
+"ali's average is : 19.5"
+
+
+# Exercise:
+'''
+برنامه ای بنویسی که 5 عدد از ورودی بگیرد و بزرگترین عدد را نمایش دهد
+
+
+
+'''
--- azar_22.py
+++ azar_22.py
@@ -13,24 +13,51 @@
 #     print(i)
 
 
-for i in range(1000, 10001, 2):
-    print(i)
-even_numbers = []
-for i in range(1000, 10001):
-    if i % 2 == 0:
-        even_numbers.append(i)
-print(even_numbers)
-print(len(even_numbers))
+# for i in range(1000, 10001, 2):
+#     print(i)
+# even_numbers = []
+# for i in range(1000, 10001):
+#     if i % 2 == 0:
+#         even_numbers.append(i)
+# print(even_numbers)
+# print(len(even_numbers))
 
 # روش بعدی بدون کمک تابع len
 
-numbers = []
-count = 0
-for i in range(1000, 10001):
-    if i % 2 == 0:
-        count = count + 1
-        numbers.append(i)
-print(count)
+# numbers = []
+# count = 0
+# for i in range(1000, 10001):
+#     if i % 2 == 0:
+#         count = count + 1
+#         numbers.append(i)
+# print(count)
 
 # exercise1 : با دو روش بالا تعداد اعداد فرد بین دو هزار تا پنجاه هزار را نمایش بده
+
+# c = 0
+# for i in range(2001, 50_000, 2):
+#     c += 1  # c = c +1
+# print(c)
+
+# numbers = []
+# for i in range(2000, 50000):
+#     if i % 2 != 0:
+#         numbers.append(i)
+# print(len(numbers))
+
 # exercise 2: مجموع اعداد زوج بین دو هزار تا 3ه هزار را حساب نمایید
+
+x = 0
+
+for i in range(2000, 3001, 2):
+    x = x + i
+print(x)
+
+numbers = []
+for i in range(2000, 3001):
+    if i % 2 == 0:
+        numbers.append(i)
+
+print(sum(numbers))
+x = [1, 2, 3]
+print(sum(x))

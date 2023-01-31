3e3ae7937f557324d1886812fd6bc08eb0ef93db
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-01-31 16:58:47 +0330

    sadeghi

---
+++ about_function.py
@@ -0,0 +1,33 @@
+# define function    تعریف تابع
+
+# def my_block():
+#     print('hello every body')
+#     print('welcome to our python class')
+#     print('we are going to learn every thing about python. from zero to hero.!!')
+
+
+# calling the function   صدازدن تابع
+# my_block()
+
+
+# def greet(name):
+#     print(f'hello {name}')
+#     print('welcome to our python class')
+#     print('we are going to learn every thing about python. from zero to hero.!!')
+
+
+# entry = input('enter your name: ')
+
+# greet(entry)
+
+
+# def add(x, y):
+#     print(x+y)
+
+
+# n1 = int(input('enter first number:'))
+# n2 = int(input('enter second number:'))
+# add(n1, n2)
+
+
+# یک تابع تعریف کن که لیستی از اعداد را بگیرد و حاصلجمع آنها را پرینت نماید

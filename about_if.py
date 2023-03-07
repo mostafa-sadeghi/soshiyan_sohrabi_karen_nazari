4db37e237d92f84d93cb7fc73640899702d26d5f
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-07 17:02:23 +0330

    sadeghi

---
+++ about_if.py
@@ -0,0 +1,163 @@
+# a = 4
+# b = 4
+
+# if a <= b:
+#     print("a is lower than or equal to b.")
+
+# if a > b:
+#     print("a is greater than b.")
+
+# if a < b or a == b:
+#     print("a is lower than or equal to b.")
+
+
+# if True or True:
+#     print("True")
+
+# if True or False:
+#     print("True")
+
+# if False or False:
+#     print("True")
+
+# if True and True:
+#     print("True")
+
+# if False and True:
+#     print("True")
+
+# if False and False:
+#     print("True")
+
+# name = input('enter a name:> ')
+# age = int(input('enter a age:> '))
+
+
+# if age >= 13  and age < 18:
+#     print(f'{name} is teenager')
+# if 13 <= age < 18:
+#     print(f'{name} is teenager')
+
+
+# a = 4
+# b = 4
+
+# if a <= b:
+#     print("a is lower than or equal to b.")
+
+# if a >= b:
+#     print("a is greater than b.")
+
+# a = 4
+# b = 4
+
+# if a == b:
+#     print("a = b")
+# if a != b:
+#     print("a is not equal to b")
+# if not a == b:
+#     print("a is not equal to b")
+
+# برنامه ای بنویسید که یک نام را از ورودی دریافت نماید
+# در صورتی که کاربر نام را وار دکرده باشد پیغام زیر را پرینت کند
+# Hello, ali
+# در صورتی که
+# exit
+# را وارد کرده باشد پیغام زیر را نمایش دهد و از برنامه خارج شود
+# good bye
+
+
+# name = input('enter your name:> ')
+# if name == 'exit':
+#     print('good bye')
+#     exit()
+
+# else:
+#     print(f'hello, {name}')
+
+# print("balalalalal")
+# print("----------------------")
+
+
+# a = True
+# if a:
+#     print("some thing...")
+
+
+# if not a:
+#     print("a is not True")
+
+
+# a = 4
+# b = 4
+
+# c = a == b
+
+# print(c)
+
+
+# if 1:
+#     print("1")
+
+# if 0:
+#     print("0")
+
+# if -1:
+#     print("-1")
+
+# if "1":
+#     print("ok")
+
+# if "":
+#     print("ok")
+
+# if " ":
+#     print("ok")
+
+# name = input('enter your name:> ')
+# age = int(input('enter your age:> '))
+
+# if age < 8:
+#     print(f'{name}, your are kid...')
+# elif age >= 8 and age < 13:
+#     print(f'{name}, your are junior...')
+# elif age >= 13 and age < 18:
+#     print(f'{name}, your are teenager...')
+# else:
+#     print(f'{name}, your are adult...')
+
+
+# 1. کدام گزینهها شرط زیر را چک می کند:
+# a < b
+# a. if a less than b:
+# b. if a < b
+# c. if a > b
+# d. if a < b:
+# e. if (a < b)
+# f. if a >= b
+# g. if a <= b:
+
+
+# 2. کدام گزینهها برابری a و b
+# را چک می کند.
+# a. if a equals b:
+# b. if a = b
+# c. if a = b:
+# d. if a == b:
+# e. if a == b
+# f. if a === b
+# g. if a === b:
+
+
+# یک عدد تک رقمی حدس بزنید
+# اگر حدس شما درست بود
+# پیغام
+# you won
+# در غیر اینصورت پیغام
+# you lose
+# را پرینت نمائید
+
+import random
+
+x = random.randrange(0, 10)
+number = input('enter a number')

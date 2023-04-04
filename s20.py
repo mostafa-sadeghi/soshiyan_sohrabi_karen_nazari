ba195fc995642f12c8a78378d4ddc457f18625f1
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-04 16:58:14 +0330

    sadeghi

---
+++ s20.py
@@ -0,0 +1,104 @@
+# for i in range(5):
+#     print("i want to learn pygame")
+#     print("and i want to learn django")
+
+# print("OK")
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(1,11):
+#     print(i, end=' ')
+# print()
+# for i in range(10):
+#     print(i+1, end=' ')
+# print()
+# for i in range(2,11,2):
+#     print(i, end=' ')
+# print()
+
+# for i in range(5):
+#     print((i+1)*2)
+
+
+# for i in range(1,10,2):
+#     print(i, end=' ')
+
+
+# for i in range(10, 0, -1):
+#     print(i, end=' ')
+
+# for i in [2,3,6,8,0,0]:
+#     print(i, end=' ')
+# for i in (2,3,6,8,0,0):
+#     print(i, end=' ')
+# for i in '2380954':
+#     print(i, end=' ')
+
+
+# for i in range(3):
+#     print("a")
+
+# for j in range(3):
+#     print("b")
+
+# for i in range(3):
+#     print("a")
+
+#     for j in range(3):
+#         print("b")
+
+
+# برنامه ای بنویسد که پنج عدد از ورودی دریافت نماید . حاصل جمع اعداد وارد شده را محاسبه و نمایش دهد
+
+# total = 0
+# for i in range(5):
+#     new_number = int(input('enter a number:> '))
+#     total += new_number
+
+# print("The total is:", total)
+
+
+# home work 1
+# برنامه ای بنویسید که 5 عدد را از ورودی دریافت نماید
+# مجموع اعداد فرد وارد شده را نمایش دهد
+# مجموع اعداد زوج وارد شده را نمایش دهد
+# اعداد وارد شده را به صورت معکوس یعنی از انتها به ابتدا نمایش دهد
+
+
+# total = 0
+# for i in range(1,101):
+#     total =  total + i
+# print(total)
+
+# count = 0
+# for i in range(5):
+#     new_number = int(input('enter a number:> '))
+#     if new_number == 0:
+#         count += 1
+
+# print("number of zeros are:", count)
+
+
+# a = 0
+# for i in range(10):
+#     a = a + 1
+# for j in range(10):
+#     a = a + 1
+# print(a)
+
+# a = 0
+# for i in range(10):
+#     a = a + 1
+#     for j in range(10):
+#         a = a + 1
+# print(a)
+
+
+# for i in range(10):
+#     print(i)
+
+i = 0
+while i < 10:
+    print(i)
+    i = i + 1  # i += 1

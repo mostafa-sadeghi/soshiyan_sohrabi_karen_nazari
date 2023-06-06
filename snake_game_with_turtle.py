f098d669e9452426cec1cbd5a4cb0e2912be7bcf
msa <msai.sadehi@gmail.com>
2023-06-06 16:57:54 +0330

    snake game before adding bodies

--- snake_game_with_turtle.py
+++ snake_game_with_turtle.py
@@ -1,16 +1,25 @@
-import turtle
+from turtle import Screen, Turtle
 import time
+from random import randint
+
+snake_body = []
+
 
 def go_up():
     if head.direction != "down":
         head.direction = "up"
+
+
 def go_down():
     if head.direction != "up":
         head.direction = "down"
 
+
 def go_left():
     if head.direction != "right":
         head.direction = "left"
+
+
 def go_right():
     if head.direction != "left":
         head.direction = "right"
@@ -31,15 +40,32 @@ def move():
         head.setx(x + 20)
 
 
-window = turtle.Screen()
+def change_food_position():
+    food_x = randint(-240, 240)
+    food_y = randint(-240, 240)
+    food.goto(food_x, food_y)
+
+
+window = Screen()
 window.title("Snake Game")
 window.bgcolor("blue")
 window.setup(width=600, height=600)
 
-head = turtle.Turtle()
+head = Turtle()
+head.speed("fastest")
 head.shape("square")
+head.color("black")
 head.penup()
 head.direction = ""
+
+
+food = Turtle()
+food.speed("fastest")
+food.shape("circle")
+food.color("red")
+food.penup()
+
+change_food_position()
 
 window.listen()
 window.onkey(go_up, "Up")
@@ -47,10 +73,13 @@ window.onkey(go_down, "Down")
 window.onkey(go_left, "Left")
 window.onkey(go_right, "Right")
 
-# TODO اضافه کردن سایر جهت ها
-
 
 while True:
     window.update()
+
+    if head.distance(food) < 20:
+        change_food_position()
+        # TODO ایجاد بدن جدید و اضافه کردن به لیست
+
     move()
     time.sleep(0.1)

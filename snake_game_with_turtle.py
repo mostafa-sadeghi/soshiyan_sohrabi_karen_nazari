151a53f44d1a53a95c7a497b6d331d7892c97c03
msa <msai.sadehi@gmail.com>
2023-06-13 16:58:30 +0330

    sadeghi

--- snake_game_with_turtle.py
+++ snake_game_with_turtle.py
@@ -3,6 +3,8 @@ import time
 from random import randint
 
 snake_body = []
+score = 0
+high_score = 0
 
 
 def go_up():
@@ -46,26 +48,45 @@ def change_food_position():
     food.goto(food_x, food_y)
 
 
+def make_turtle(turtle_shape, turtle_color):
+    my_turtle = Turtle()
+    my_turtle.speed("fastest")
+    my_turtle.shape(turtle_shape)
+    my_turtle.color(turtle_color)
+    my_turtle.penup()
+    return my_turtle
+
+
+def reset():
+    head.goto(0, 0)
+    head.direction = ""
+    for body in snake_body:
+        body.hideturtle()
+
+    snake_body.clear()
+
+
 window = Screen()
 window.title("Snake Game")
 window.bgcolor("blue")
 window.setup(width=600, height=600)
+window.tracer(0)
 
-head = Turtle()
-head.speed("fastest")
-head.shape("square")
-head.color("black")
-head.penup()
+
+head = make_turtle("square", "black")
 head.direction = ""
 
 
-food = Turtle()
-food.speed("fastest")
-food.shape("circle")
-food.color("red")
-food.penup()
+food = make_turtle("circle", "red")
 
 change_food_position()
+
+score_pen = make_turtle("square", "white")
+score_pen.goto(0, 260)
+score_pen.hideturtle()
+score_pen.write(
+    f"Score : {score}, High Score: {high_score}", align="center", font=38)
+
 
 window.listen()
 window.onkey(go_up, "Up")
@@ -79,7 +100,26 @@ while True:
 
     if head.distance(food) < 20:
         change_food_position()
-        # TODO ایجاد بدن جدید و اضافه کردن به لیست
+        # TODO اضافه کردن امتیاز
+        # score_pen.clear()
+
+        new_body = make_turtle("square", "grey")
+        snake_body.append(new_body)
+
+    for i in range(len(snake_body) - 1, 0, -1):
+        x_prev = snake_body[i-1].xcor()
+        y_prev = snake_body[i-1].ycor()
+        snake_body[i].goto(x_prev, y_prev)
+    if len(snake_body) > 0:
+        x_head = head.xcor()
+        y_head = head.ycor()
+        snake_body[0].goto(x_head, y_head)
+
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        reset()
 
     move()
-    time.sleep(0.1)
+
+
+    # TODO در صورت برخورد به بدن ببازد
+    time.sleep(0.2)
